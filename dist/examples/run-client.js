144065909c60971aaf599bc85bdd9f8346df118a
edgracilla <ed.gracilla@gmail.com>
2022-08-19 19:31:05 +0800

    zmdp-protocoder for dev

--- dist/examples/run-client.js
+++ dist/examples/run-client.js
@@ -1,31 +1,33 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const protobuf = require('protobufjs');
+const zmdp_protocoder_1 = __importDefault(require("zmdp-protocoder"));
 const index_1 = require("../index");
+const zpc = new zmdp_protocoder_1.default('./examples/zproto');
 const client = new index_1.Client({
     address: 'tcp://127.0.0.1:4000',
     timeout: 1000 * 15
 });
-const main = async () => {
-    const proto = await protobuf.load('access.proto');
-    const ReadParams = proto.lookupType('access.read');
-    const payload = {
-        _id: 'tester-tester',
-        meta: {
-            _perm: { readOwned: false },
-            _access: { _id: 'accessId' },
-            _user: { _id: 'userId', name: 'userName' },
-        }
-    };
-    const errMsg = ReadParams.verify(payload);
-    if (errMsg) {
-        throw Error(errMsg);
+const mod = 'access';
+const svc = 'tu-identity-api';
+const payload = {
+    _id: 'tester-tester',
+    meta: {
+        _perm: { readOwned: false },
+        _access: { _id: 'accessId' },
+        _user: { _id: 'userId', name: 'userName' },
     }
-    const msg = ReadParams.create(payload);
-    const buf = ReadParams.encode(msg).finish();
-    const resp = await client.sendRcv('tu-identity-api', 'access', 'read', buf);
+};
+const main = async () => {
+    const buf = await zpc.paramEncode(mod, 'read', payload);
+    const resp = await client.sendRcv(svc, mod, 'read', buf);
+    if (resp) {
+        const { result } = await zpc.resultDecode(mod, 'read', resp);
+        console.log('--a', result, '--b');
+    }
     // const resp = await client.sendRcv('tu-identity-api', 'access', 'testPassSupportedTypes', 'str', '1', 'true', '{"foo":"bar"}')
-    console.log(resp === null || resp === void 0 ? void 0 : resp.toString());
 };
 // setInterval(main, 1000);
 main();
--- examples/run-client.ts
+++ examples/run-client.ts
@@ -1,38 +1,35 @@
-const protobuf = require('protobufjs')
+import ProtoCoder from 'zmdp-protocoder'
 import { Client } from '../index'
+
+const zpc = new ProtoCoder('./examples/zproto')
 
 const client = new Client({
   address: 'tcp://127.0.0.1:4000',
   timeout: 1000 * 15
 });
 
+const mod = 'access'
+const svc = 'tu-identity-api'
+
+const payload = {
+  _id: 'tester-tester',
+  meta: {
+    _perm: { readOwned: false },
+    _access: { _id: 'accessId' },
+    _user: { _id: 'userId', name: 'userName' },
+  }
+}
+
 const main = async () => {
-  const proto = await protobuf.load('access.proto')
-  const ReadParams = proto.lookupType('access.read')
+  const buf = await zpc.paramEncode(mod, 'read', payload)
+  const resp = await client.sendRcv(svc, mod, 'read', buf)
 
-  const payload = {
-    _id: 'tester-tester',
-    meta: {
-      _perm: { readOwned: false },
-      _access: { _id: 'accessId' },
-      _user: { _id: 'userId', name: 'userName' },
-    }
+  if (resp) {
+    const { result } = await zpc.resultDecode(mod, 'read', resp)
+    console.log('--a', result, '--b')
   }
-
-  const errMsg = ReadParams.verify(payload)
-
-  if (errMsg) {
-    throw Error(errMsg)
-  }
-
-  const msg = ReadParams.create(payload)
-  const buf = ReadParams.encode(msg).finish()
-
-  const resp = await client.sendRcv('tu-identity-api', 'access', 'read', buf)
-
+  
   // const resp = await client.sendRcv('tu-identity-api', 'access', 'testPassSupportedTypes', 'str', '1', 'true', '{"foo":"bar"}')
-
-  console.log(resp?.toString())
 }
 
 // setInterval(main, 1000);
